7854abfb7b5a7fee74150e484e67fdb188b3e6c3
viralsampat-multidots <sampatviral90@gmail.com>
2025-10-08 15:41:19 +0530

    Change file

--- lib/sanity-image.ts
+++ lib/sanity-image.ts
@@ -1,4 +1,5 @@
 import createImageUrlBuilder from '@sanity/image-url'
+import type { Image } from 'sanity'
 import { client } from './sanity-client'
 
-export const urlFor = (source: any) => createImageUrlBuilder(client).image(source)
+export const urlFor = (source: Image) => createImageUrlBuilder(client).image(source)
